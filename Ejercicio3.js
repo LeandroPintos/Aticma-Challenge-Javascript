819d320e51990c46f553b6cdc86e9f5e441fc41d
Leandro Pintos <leandro.nahuel.pintos@gmail.com>
2021-10-14 13:01:28 -0300

    Ultimo Cambio

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,8 +1,8 @@
-function ejercicio3(){
+function cargarArrayRandom(){
     let N = 0;
     let array = [];
 
-    N =  prompt('Ingrese numero');
+    N =  prompt('Ingrese la cantidad de numeros que desee');
     for(let i = 0; i< N; i++){
         array[i] = Math.trunc(Math.random() * 1000);
     }
